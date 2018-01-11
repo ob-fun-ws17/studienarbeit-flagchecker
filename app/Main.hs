2913c457ef77c8e03b5318b44355390c85f8e762
Leon Kladnik <Leon Kladnik>
2018-01-11 14:47:51 +0100

    Basic functionality. Not that much actually.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
 module Main where
 
+import System.IO (hSetBuffering, BufferMode(NoBuffering), stdout)
+import System.Exit
+import System.Directory
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  hSetBuffering stdout NoBuffering
+  dir <- listDirectory "."
+  cmd <- getLine
+  case cmd of
+    "filter" -> putStrLn $ unlines (filterFileTyp "txt" dir)
+    otherwise -> exitSuccess
+
+
+--import System.IO
+--import Data.Char(toUpper)
+
+--main :: IO ()
+--main = do
+--       inh <- openFile "input.txt" ReadMode
+--       outh <- openFile "output.txt" WriteMode
+--       mainloop inh outh
+--       hClose inh
+--       hClose outh
+
+-- mainloop :: Handle -> Handle -> IO ()
+-- mainloop inh outh =
+--    do ineof <- hIsEOF inh
+--       if ineof
+--           then return ()
+--           else do inpStr <- hGetLine inh
+--                   hPutStrLn outh (map toUpper inpStr)
+--                   mainloop inh outh
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,24 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Directory
+import Data.List.Split
+import Control.Monad (filterM)
+-- listDir :: FilePath -> IO [FilePath]
+-- listDir path = listDirectory path
+
+getEnding :: FilePath -> String
+getEnding file = last (splitOn "." file)
+
+filterFileTyp :: String -> [FilePath] -> [FilePath]
+filterFileTyp ending files = filter ((== ending) . getEnding) files
+
+filterDirs :: [FilePath] -> IO [FilePath]
+filterDirs files = filterM doesDirectoryExist files
+
+wholeDirRecursive :: FilePath -> FilePath -> IO ()
+wholeDirRecursive src dst =
+  do
+    files <- listDirectory src
+    filtered <- filterM (fmap not . doesDirectoryExist) files
+    createDirectoryIfMissing True dst
+    mapM_ (`copyFile` dst) filtered
