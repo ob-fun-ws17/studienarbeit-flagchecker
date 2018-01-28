9b9886f6c65a4344799e3b042790f494fa5061f2
Leon Kladnik <Leon Kladnik>
2018-01-28 21:43:19 +0100

    Functional version of the CopyManager. Implemented recursive folder copy and functional "mass multi copy". Mostly done. Next iteration will be with test cases and probably more exception handling.

--- app/Main.hs
+++ app/Main.hs
@@ -4,33 +4,61 @@ import System.IO (hSetBuffering, BufferMode(NoBuffering), stdout)
 import System.Exit
 import System.Directory
 import Lib
+import Data.List.Split
 
+-- | Entry point for the application.
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  dir <- listDirectory "."
+  putStrLn "Please enter a valid command."
+  putStrLn "Type help for a list of available commands."
   cmd <- getLine
-  case cmd of
-    "filter" -> putStrLn $ unlines (filterFileTyp "txt" dir)
-    otherwise -> exitSuccess
+  let args = splitOn " " cmd
+  let option = head args
+  case (option) of
+    "help" -> printOptions
+    "file" -> copyFileToDst (args!!1) (args!!2)
+    "folder" -> copyDir (args!!1) (args!!2)
+    "filter" -> filteredCopy (args!!1) (args!!2) (args!!3)
+    "multi" -> multi
+    "massMulti" -> massMulti
 
+-- | Helps to call the multi function.
+multi :: IO ()
+multi = do
+  putStrLn "Enter file to copy:"
+  src <- getLine
+  putStrLn "Enter dst directory:"
+  dst <- getLine
+  putStrLn "Enter number of copies:"
+  copies <- getLine
+  let value = read copies :: Int
+  multiCopy src dst 0 value
 
---import System.IO
---import Data.Char(toUpper)
+-- | Helps to call the massMultiCopy function.
+massMulti :: IO ()
+massMulti = do
+  putStrLn "Enter src directory:"
+  src <- getLine
+  putStrLn "Enter dst directory:"
+  dst <- getLine
+  putStrLn "Enter ending to copy:"
+  ending <- getLine
+  putStrLn "Enter number of copies:"
+  copies <- getLine
+  let value = read copies :: Int
+  massMultiCopy src dst ending value
 
---main :: IO ()
---main = do
---       inh <- openFile "input.txt" ReadMode
---       outh <- openFile "output.txt" WriteMode
---       mainloop inh outh
---       hClose inh
---       hClose outh
-
--- mainloop :: Handle -> Handle -> IO ()
--- mainloop inh outh =
---    do ineof <- hIsEOF inh
---       if ineof
---           then return ()
---           else do inpStr <- hGetLine inh
---                   hPutStrLn outh (map toUpper inpStr)
---                   mainloop inh outh
+-- | Prints available commands.
+printOptions :: IO ()
+printOptions = do
+  putStrLn "file      - copy a file"
+  putStrLn "usage:    file <src> <dst>"
+  putStrLn "folder    - copy a folder"
+  putStrLn "usage:    folder <src> <dst>"
+  putStrLn "filter    - copy all files with a specific ending"
+  putStrLn "usage:    filter <src> <dst> <ending>"
+  putStrLn "multi     - copy a file multiple times"
+  putStrLn "usage:    multi (will prompt you again)"
+  putStrLn "massMulti - copy all files with a specific ending multiple times"
+  putStrLn "usage:    massMulti (will prompt you again)"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,57 +1,97 @@
+-- | The lib of the project
 module Lib where
 
 import System.Directory
 import Data.List.Split
-import Control.Monad (filterM)
+import Control.Monad (filterM, when, forM_)
+import Control.Applicative((<$>))
+import Control.Exception(throw)
 -- listDir :: FilePath -> IO [FilePath]
 -- listDir path = listDirectory path
 
+-- | Extracts the file extension from a path.
 getEnding :: FilePath -> String
 getEnding file = last (splitOn "." file)
 
+-- | Extracts the last part of the path, so the actual filename.
 getFileName :: FilePath -> String
 getFileName path = last (splitOn "/" path)
 
+-- | Filters the files without the proper ending.
 filterFileTyp :: String -> [FilePath] -> [FilePath]
 filterFileTyp ending files = filter ((== ending) . getEnding) files
 
+-- | Only returns a list of the directories.
 filterDirs :: [FilePath] -> IO [FilePath]
 filterDirs files = filterM doesDirectoryExist files
 
-wholeDirRecursive :: FilePath -> FilePath -> IO ()
-wholeDirRecursive src dst = -- does not work right now ___ dst name needs to be generated from src name + dst path
-  do
-    files <- listDirectory src
-    filtered <- filterM (fmap not . doesDirectoryExist) files
-    createDirectoryIfMissing True dst
-    mapM_ (`copyFile` dst) filtered
+-- -- | Doesn't work right now. Should copy the whole directory recursive.
+-- wholeDirRecursive :: FilePath -> FilePath -> IO ()
+-- wholeDirRecursive src dst = -- does not work right now ___ dst name needs to be generated from src name + dst path
+--   do
+--     files <- listDirectory src
+--     filtered <- filterM (fmap not . doesDirectoryExist) files
+--     createDirectoryIfMissing True dst
+--     mapM_ (`copyFile` dst) filtered
 
 
+-- | Copies the file to the specified location.
 copyFileToDst :: FilePath -> FilePath -> IO ()
 copyFileToDst src dst =
   do
     createDirectoryIfMissing True dst
     copyFile src (dst ++ "/" ++ (getFileName src))
 
-copyDirToDst :: FilePath -> FilePath -> IO ()
-copyDirToDst src dst =
-  do
-    files <- listDirectory src
-    filtered <- filterM (fmap not . doesDirectoryExist) files
-    folders <- filterM doesDirectoryExist files
-    --foldersAndNames = zip (map getFileName folders) folders
-    createDirectoryIfMissing True dst
-    mapM_ (`copyFileToDst` dst) filtered
-    -- source = map (src ++ "/" ++) folders
-    -- target = map (dst ++ "/" ++) folders
-    -- mapM_ (copyDirToDst src dst) folders --foldersAndNames(sour)
+-- -- | Copies a whole directory to the specified location. Doesn't work recursively right now.
+-- copyDirToDst :: FilePath -> FilePath -> IO ()
+-- copyDirToDst src dst =
+--   do
+--     files <- listDirectory src
+--     filtered <- filterM (fmap not . doesDirectoryExist) files
+--     folders <- filterM doesDirectoryExist files
+--     --foldersAndNames = zip (map getFileName folders) folders
+--     createDirectoryIfMissing True dst
+--     mapM_ (`copyFileToDst` dst) filtered
+--     forM_ folders $ \name -> do
+--       let srcPath = src ++ "/" ++ name
+--       let dstPath = dst ++ "/" ++ name
+--       createDirectoryIfMissing True dstPath
+--       copyDirToDst srcPath dstPath
 
+-- | Taken from StackOverflow. Copies a whole Dir. Target directory will be created.
+-- | Fails if the destination directory already exists.
+copyDir ::  FilePath -> FilePath -> IO ()
+copyDir src dst = do
+  whenM (not <$> doesDirectoryExist src) $
+    throw (userError "source does not exist")
+  whenM (doesFileOrDirectoryExist dst) $
+    throw (userError "destination already exists")
+
+  createDirectory dst
+  content <- getDirectoryContents src
+  let xs = filter (`notElem` [".", ".."]) content
+  forM_ xs $ \name -> do
+    let srcPath = src ++ "/" ++ name
+    let dstPath = dst ++ "/" ++ name
+    isDirectory <- doesDirectoryExist srcPath
+    if isDirectory
+      then copyDir srcPath dstPath
+      else copyFile srcPath dstPath
+
+  where
+    doesFileOrDirectoryExist x = orM [doesDirectoryExist x, doesFileExist x]
+    orM xs = or <$> sequence xs
+    whenM s r = s >>= flip when r
+
+
+-- | Copies all the files with a specific ending from src to dst.
 filteredCopy :: FilePath -> FilePath -> String -> IO ()
 filteredCopy src dst ending =
   do
     files <- listDirectory src
     mapM_ (`copyFileToDst` dst) (filterFileTyp ending files)
 
+-- | Copies a file multiple times. It will be named after the iteration.
 multiCopy :: FilePath -> FilePath -> Int -> Int -> IO ()
 multiCopy src dst iteration goal =
   do
@@ -62,23 +102,20 @@ multiCopy src dst iteration goal =
     else do
       return ()
 
--- multiCopyAssist :: FilePath -> FilePath -> (Int, Int) -> IO ()
--- multiCopyAssist src dst (iteration, goal) =
---   do
---     if iteration < goal then do
---       copyFileToDst src dst
---       renameFile (dst ++ "/" ++ (getFileName src)) (dst ++ "/" ++ show iteration ++ "." ++ (getEnding src))
---       multiCopyAssist src dst ((iteration + 1), goal)
---     else do
---       return ()
+-- | Makes multiple copies of the specified filetype. Absolutely unique feature.
+massMultiCopy :: FilePath -> FilePath -> String -> Int -> IO ()
+massMultiCopy src dst ending copyCount =
+  do
+    files <- listDirectory src
+    let filtered = filterFileTyp ending files
+    let subGoals = map (*copyCount) [1..]
+    let iterationStart = map (subtract copyCount) subGoals
+    let startAndEnd = zip iterationStart subGoals
+    let dstList = repeat (dst ++ "/" ++ (getFileName src))
+    let sourceAndDst = zip filtered dstList
+    let tuplelist = zip sourceAndDst startAndEnd
+    mapM_ multiCopyCaller tuplelist
 
--- massMultiCopy :: FilePath -> FilePath -> String -> Int -> IO ()
--- massMultiCopy src dst ending copyCount =
---   do
---     files <- listDirectory src
---     filtered = filterFileTyp ending files
---     --subGoalSize = (length filtered) * copyCount
---     subGoals = [1..] * copyCount
---     fileAndGoal = zip subGoals filtered
---     filter (multicopy (src ++ "/" ++ getFileName) (dst ++ "/" ++ getFileName) ) fileAndGoal
---     mapM_ (multicopy src dst )
+-- | Helps to call multiCopy with the appropriate values.
+multiCopyCaller :: ((FilePath, FilePath), (Int, Int)) -> IO ()
+multiCopyCaller ((src, dst), (iteration, goal)) = multiCopy src dst iteration goal
