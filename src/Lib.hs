d07aac5264983c03ef49189f9a5d16ca2e0498a1
Leon Kladnik <Leon Kladnik>
2018-01-28 16:42:55 +0100

    Basic functionality. FileCopy FolderCopy (not recursive) FilteredCopy (by file extension) MultiCopy (multiple copies of the same file; right now, only works for one file at a time)
    Next iteration will be with CLI.

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,6 +9,9 @@ import Control.Monad (filterM)
 getEnding :: FilePath -> String
 getEnding file = last (splitOn "." file)
 
+getFileName :: FilePath -> String
+getFileName path = last (splitOn "/" path)
+
 filterFileTyp :: String -> [FilePath] -> [FilePath]
 filterFileTyp ending files = filter ((== ending) . getEnding) files
 
@@ -22,3 +25,60 @@ wholeDirRecursive src dst = -- does not work right now ___ dst name needs to be 
     filtered <- filterM (fmap not . doesDirectoryExist) files
     createDirectoryIfMissing True dst
     mapM_ (`copyFile` dst) filtered
+
+
+copyFileToDst :: FilePath -> FilePath -> IO ()
+copyFileToDst src dst =
+  do
+    createDirectoryIfMissing True dst
+    copyFile src (dst ++ "/" ++ (getFileName src))
+
+copyDirToDst :: FilePath -> FilePath -> IO ()
+copyDirToDst src dst =
+  do
+    files <- listDirectory src
+    filtered <- filterM (fmap not . doesDirectoryExist) files
+    folders <- filterM doesDirectoryExist files
+    --foldersAndNames = zip (map getFileName folders) folders
+    createDirectoryIfMissing True dst
+    mapM_ (`copyFileToDst` dst) filtered
+    -- source = map (src ++ "/" ++) folders
+    -- target = map (dst ++ "/" ++) folders
+    -- mapM_ (copyDirToDst src dst) folders --foldersAndNames(sour)
+
+filteredCopy :: FilePath -> FilePath -> String -> IO ()
+filteredCopy src dst ending =
+  do
+    files <- listDirectory src
+    mapM_ (`copyFileToDst` dst) (filterFileTyp ending files)
+
+multiCopy :: FilePath -> FilePath -> Int -> Int -> IO ()
+multiCopy src dst iteration goal =
+  do
+    if iteration < goal then do
+      copyFileToDst src dst
+      renameFile (dst ++ "/" ++ (getFileName src)) (dst ++ "/" ++ show iteration ++ "." ++ (getEnding src))
+      multiCopy src dst (iteration + 1) goal
+    else do
+      return ()
+
+-- multiCopyAssist :: FilePath -> FilePath -> (Int, Int) -> IO ()
+-- multiCopyAssist src dst (iteration, goal) =
+--   do
+--     if iteration < goal then do
+--       copyFileToDst src dst
+--       renameFile (dst ++ "/" ++ (getFileName src)) (dst ++ "/" ++ show iteration ++ "." ++ (getEnding src))
+--       multiCopyAssist src dst ((iteration + 1), goal)
+--     else do
+--       return ()
+
+-- massMultiCopy :: FilePath -> FilePath -> String -> Int -> IO ()
+-- massMultiCopy src dst ending copyCount =
+--   do
+--     files <- listDirectory src
+--     filtered = filterFileTyp ending files
+--     --subGoalSize = (length filtered) * copyCount
+--     subGoals = [1..] * copyCount
+--     fileAndGoal = zip subGoals filtered
+--     filter (multicopy (src ++ "/" ++ getFileName) (dst ++ "/" ++ getFileName) ) fileAndGoal
+--     mapM_ (multicopy src dst )
